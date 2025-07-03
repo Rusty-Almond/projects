8aaa613cd11bb0fcc4a38c6ca0b34509bee073c1
Rusty-Almond <rustyalmond@duck.com>
2025-07-03 21:42:51 +0530

    changes

--- watermelon.java
+++ watermelon.java
@@ -5,7 +5,7 @@ class watermelon
     int w;
     w=sc.nextInt();
     if(w>=1&&w<=100)
-     { if(w%2==0 && (w/2)%2==0)
+     { if(w%2==0 && w!=2)
         System.out.println("YES");
        else 
         System.out.println("NO");
