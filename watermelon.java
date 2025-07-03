62b88f8fe0886594f3ab6c47ad82d55ed9d8ff0a
Rusty-Almond <rustyalmond@duck.com>
2025-07-03 20:37:03 +0530

    first commit

---
+++ watermelon.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class watermelon
+{ public static void main(String args[])
+  { Scanner sc=new Scanner(System.in);
+    int w;
+    w=sc.nextInt();
+    if(w>=1&&w<=100)
+     { if(w%2==0 && (w/2)%2==0)
+        System.out.println("YES");
+       else 
+        System.out.println("NO");
+     }
+    else
+     System.out.println("Wrong Input");
+    }
+}
