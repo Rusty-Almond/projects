489923f27eaacc037c35b0b3a38177cff1e0743b
Rusty-Almond <rustyalmond@duck.com>
2025-09-12 20:22:51 +0530

    https://codeforces.com/contest/2140/problem/B

---
+++ cpp/anotherdivisibilityproblem.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{ ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >>t;
+  long long x;
+  while(t-->0)
+   { cin >> x;
+     cout << 2*x <<endl; //8x will also do
+   }
+}
